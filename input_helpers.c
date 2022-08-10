5514ce8160463c3f65d044a873be51b13a19f8ba
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-08-10 09:06:40 +0100

    Update input_helpers.c

--- input_helpers.c
+++ input_helpers.c
@@ -7,27 +7,27 @@ int handle_args(int *exe_ret);
 int check_args(char **args);
 
 /**
-* get_args - Gets a command from standard input .
-* @line: A buffer to store the command .
-* @exe_ret: The return value of the last executed command.
-*
-* Return: If an error occurs - NULL.
-*         Otherwise - a pointer to the stored command.
-*/
+ * get_args - Gets a command from standard input.
+ * @line: A buffer to store the command.
+ * @exe_ret: The return value of the last executed command.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the stored command.
+ */
 char *get_args(char *line, int *exe_ret)
 {
-size_t n = 0;
-ssize_t read;
-char *prompt = "$ ";
+	size_t n = 0;
+	ssize_t read;
+	char *prompt = "$ ";
 
-if (line)
-free(line);
+	if (line)
+		free(line);
 
-read = _getline(&line, &n, STDIN_FILENO);
-if (read == -1)
-return (NULL);
-if (read == 1)
-{
+	read = _getline(&line, &n, STDIN_FILENO);
+	if (read == -1)
+		return (NULL);
+	if (read == 1)
+	{
 		hist++;
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, prompt, 2);
@@ -135,7 +135,7 @@ int run_args(char **args, char **front, int *exe_ret)
 }
 
 /**
- * handle_args - Gets,calls,and runs the execution of a command.
+ * handle_args - Gets, calls, and runs the execution of a command.
  * @exe_ret: The return value of the parent process' last executed command.
  *
  * Return: If an end-of-file is read - END_OF_FILE (-2).
@@ -171,38 +171,39 @@ int handle_args(int *exe_ret)
 			args[index] = NULL;
 			ret = call_args(args, front, exe_ret);
 			args = &args[++index];
-		index = 0;
-}
-}
+			index = 0;
+		}
+	}
 	if (args)
-	ret = call_args(args, front, exe_ret);
+		ret = call_args(args, front, exe_ret);
 
-free(front);
-return (ret);
+	free(front);
+	return (ret);
 }
 
 /**
-* check_args - Checks if there are any leading ';', ';;', '&&', or '||'.
-* @args: 2D pointer to tokenized commands and arguments.
-*
-* Return: If a ';', '&&', or '||' is placed at an invalid position - 2.
-*	   Otherwise - 0.
-*/
+ * check_args - Checks if there are any leading ';', ';;', '&&', or '||'.
+ * @args: 2D pointer to tokenized commands and arguments.
+ *
+ * Return: If a ';', '&&', or '||' is placed at an invalid position - 2.
+ *	   Otherwise - 0.
+ */
 int check_args(char **args)
 {
-size_t i;
-char *cur, *nex;
-for (i = 0; args[i]; i++)
-{
-cur = args[i];
-if (cur[0] == ';' || cur[0] == '&' || cur[0] == '|')
-{
-if (i == 0 || cur[1] == ';')
-return (create_error(&args[i], 2));
-nex = args[i + 1];
-if (nex && (nex[0] == ';' || nex[0] == '&' || nex[0] == '|'))
-return (create_error(&args[i + 1], 2));
-}
-}
-return (0);
+	size_t i;
+	char *cur, *nex;
+
+	for (i = 0; args[i]; i++)
+	{
+		cur = args[i];
+		if (cur[0] == ';' || cur[0] == '&' || cur[0] == '|')
+		{
+			if (i == 0 || cur[1] == ';')
+				return (create_error(&args[i], 2));
+			nex = args[i + 1];
+			if (nex && (nex[0] == ';' || nex[0] == '&' || nex[0] == '|'))
+				return (create_error(&args[i + 1], 2));
+		}
+	}
+	return (0);
 }
